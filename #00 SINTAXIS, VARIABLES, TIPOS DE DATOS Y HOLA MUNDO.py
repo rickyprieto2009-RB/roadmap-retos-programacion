69a843791b32fa67037f33d34d73e33b3286fa5d
rickyprieto2009-RB <rickyprieto2009@gmail.com>
2025-09-03 14:33:35 -0400

    Add files via upload

---
+++ #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  2 14:51:48 2025
+
+@author: rprieto
+"""
+
+# Ejercicio 1: SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+
+# a) Lenguaje de programación elegido:
+# https://www.python.org/
+
+# b) Comentario en una línea
+# Comentario en una línea se realiza con # al inicio, un comentario no se ejecuta
+
+# c) Comentario en varias líneas
+''' El uso de triple comilla simple ('),
+permite realizar comentarios, para esto se deben abrir y cerrar
+con triple comilla simple (').
+'''
+
+""" El uso de triple comilla doble ("),
+permite realizar comentarios, para esto se deben abrir y cerrar
+con triple comilla doble (")
+"""
+
+# d) Creación de una variable y una constante
+variable = "variable 1"
+variable = "nuevo valor variable 1"
+
+constante = "constante 1"
+
+# e) Crear variables con los tipos de datos primitivos
+int = 25
+float = 1.25
+booleano = True
+booleano = False
+string = "Cadena de texto"
+nuevo_string = "Otra cadena de texto"
+
+# f) Imprime por terminal de texto "¡Hola Python!"
+print = "¡Hola Python!"
+
+print(type(int))        # Si se puede imprimir el dato
+print(type(float))      # Si se puede imprimir el dato
+print(type(booleano))   # No se puede imprimir str
+print(type(string))     # No se puede imprimir str
